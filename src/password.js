e0d0723dda15a47d3ca2e8691f59ed8570df281c
pierce-miller <114310142+pierce-miller@users.noreply.github.com>
2024-01-18 19:54:20 -0500

    Update password.js

--- src/password.js
+++ src/password.js
@@ -8,98 +8,50 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var path = require('path');
-var crypt = require('crypto');
-var util = require('util');
-var bcrypt = require('bcryptjs');
-var fork = require('./meta/debugFork');
+const path = require('path');
+const crypt = require('crypto');
+const util = require('util');
+const bcrypt = require('bcryptjs');
+const fork = require('./meta/debugFork');
 function forkChild(message, callback) {
-    var child = fork(path.join(__dirname, 'password'));
-    child.on('message', function (msg) {
+    const child = fork(path.join(__dirname, 'password'));
+    child.on('message', (msg) => {
         callback(msg.err ? new Error(msg.err) : null, msg.result);
     });
-    child.on('error', function (err) {
+    child.on('error', (err) => {
         console.error(err.stack);
         callback(err);
     });
     child.send(message);
 }
-var forkChildAsync = util.promisify(forkChild);
+const forkChildAsync = util.promisify(forkChild);
 exports.hash = function (rounds, password) {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    password = crypt.createHash('sha512').update(password).digest('hex');
-                    return [4 /*yield*/, forkChildAsync({ type: 'hash', rounds: rounds, password: password })];
-                case 1: return [2 /*return*/, _a.sent()];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        password = crypt.createHash('sha512').update(password).digest('hex');
+        return yield forkChildAsync({ type: 'hash', rounds: rounds, password: password });
     });
 };
 exports.compare = function (password, hash, shaWrapped) {
-    return __awaiter(this, void 0, void 0, function () {
-        var fakeHash;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, getFakeHash()];
-                case 1:
-                    fakeHash = _a.sent();
-                    if (shaWrapped) {
-                        password = crypt.createHash('sha512').update(password).digest('hex');
-                    }
-                    return [4 /*yield*/, forkChildAsync({ type: 'compare', password: password, hash: hash || fakeHash })];
-                case 2: return [2 /*return*/, _a.sent()];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        const fakeHash = yield getFakeHash();
+        if (shaWrapped) {
+            password = crypt.createHash('sha512').update(password).digest('hex');
+        }
+        return yield forkChildAsync({ type: 'compare', password: password, hash: hash || fakeHash });
     });
 };
-var fakeHashCache;
+let fakeHashCache;
 function getFakeHash() {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    if (fakeHashCache) {
-                        return [2 /*return*/, fakeHashCache];
-                    }
-                    return [4 /*yield*/, exports.hash(12, Math.random().toString())];
-                case 1:
-                    fakeHashCache = _a.sent();
-                    return [2 /*return*/, fakeHashCache];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        if (fakeHashCache) {
+            return fakeHashCache;
+        }
+        fakeHashCache = yield exports.hash(12, Math.random().toString());
+        return fakeHashCache;
     });
 }
 // child process
-process.on('message', function (msg) {
+process.on('message', (msg) => {
     if (msg.type === 'hash') {
         tryMethod(hashPassword, msg);
     }
@@ -108,53 +60,29 @@ process.on('message', function (msg) {
     }
 });
 function tryMethod(method, msg) {
-    return __awaiter(this, void 0, void 0, function () {
-        var result, err_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 2, 3, 4]);
-                    return [4 /*yield*/, method(msg)];
-                case 1:
-                    result = _a.sent();
-                    process.send({ result: result });
-                    return [3 /*break*/, 4];
-                case 2:
-                    err_1 = _a.sent();
-                    process.send({ err: err_1.message });
-                    return [3 /*break*/, 4];
-                case 3:
-                    process.disconnect();
-                    return [7 /*endfinally*/];
-                case 4: return [2 /*return*/];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const result = yield method(msg);
+            process.send({ result: result });
+        }
+        catch (err) {
+            process.send({ err: err.message });
+        }
+        finally {
+            process.disconnect();
+        }
     });
 }
 function hashPassword(msg) {
-    return __awaiter(this, void 0, void 0, function () {
-        var salt, hash;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, bcrypt.genSalt(parseInt(msg.rounds, 10))];
-                case 1:
-                    salt = _a.sent();
-                    return [4 /*yield*/, bcrypt.hash(msg.password, salt)];
-                case 2:
-                    hash = _a.sent();
-                    return [2 /*return*/, hash];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        const salt = yield bcrypt.genSalt(parseInt(msg.rounds, 10));
+        const hash = yield bcrypt.hash(msg.password, salt);
+        return hash;
     });
 }
 function compare(msg) {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, bcrypt.compare(String(msg.password || ''), String(msg.hash || ''))];
-                case 1: return [2 /*return*/, _a.sent()];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield bcrypt.compare(String(msg.password || ''), String(msg.hash || ''));
     });
 }
 require('./promisify')(exports);
